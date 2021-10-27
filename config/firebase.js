e77fd98b61df516991eae49d66cf133ec1367bd7
Nicolás Russo <nicolas.russo.ortiz@gmail.com>
2021-10-27 10:54:14 -0300

    fix: change environment variable names to match usage

--- config/firebase.js
+++ config/firebase.js
@@ -4,18 +4,18 @@ import "firebase/firestore";
 import Constants from "expo-constants";
 
 const firebaseConfig = {
-	apiKey: Constants.manifest.extra.apiKey,
-	authDomain: Constants.manifest.extra.authDomain,
-	projectId: Constants.manifest.extra.projectId,
-	storageBucket: Constants.manifest.extra.storageBucket,
-	messagingSenderId: Constants.manifest.extra.messagingSenderId,
-	appId: Constants.manifest.extra.appId,
+  apiKey: Constants.manifest.extra.firebase.apiKey,
+  authDomain: Constants.manifest.extra.firebase.authDomain,
+  projectId: Constants.manifest.extra.firebase.projectId,
+  storageBucket: Constants.manifest.extra.firebase.storageBucket,
+  messagingSenderId: Constants.manifest.extra.firebase.messagingSenderId,
+  appId: Constants.manifest.extra.firebase.appId,
 };
 
 console.log("Initializing Firebase...");
 const Firebase = !firebase.apps.length
-	? firebase.initializeApp(firebaseConfig)
-	: firebase.app();
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
 
 const db = firebase.firestore();
 
